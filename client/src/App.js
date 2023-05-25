5baa93c8e3c62579aabdf1fb09da952f34895e1f
RizioDev <fabrizioalderete23@gmail.com>
2023-05-25 16:02:06 -0300

    deploy

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,9 @@ import Detail from "./components/Detail/Detail.jsx";
 import ActivityCreate from "./components/ActivityCreate/ActivityCreate.jsx";
 import WithoutNav from "./components/NavBar/WithoutNav";
 import WithNav from "./components/NavBar/WithNav";
+import axios from "axios";
+
+axios.defaults.baseURL = "http://localhost:3001/";
 
 export default function App() {
   return (
--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,139 +1,139 @@
-import axios from 'axios'
+import axios from "axios";
 
-export function getCountries (order) {
+export function getCountries(order) {
   return async function (dispatch) {
-    var json = await axios.get(`http://localhost:3001/countries?order=${order}`)
+    var json = await axios.get(`/countries?order=${order}`);
     return dispatch({
-      type: 'GET_COUNTRIES',
-      payload: json.data
-    })
-  }
+      type: "GET_COUNTRIES",
+      payload: json.data,
+    });
+  };
 }
 
-export function getNameCountryForm (name) {
+export function getNameCountryForm(name) {
   return async function (dispatch) {
     try {
-      var json = await axios.get('http://localhost:3001/countries?name=' + name)
-      return dispatch({ type: 'GET_NAME_COUNTRY_FORM', payload: json.data })
+      var json = await axios.get("/countries?name=" + name);
+      return dispatch({ type: "GET_NAME_COUNTRY_FORM", payload: json.data });
     } catch (error) {
       return dispatch({
-        type: 'FAILURE',
-        payload: error.response.data.msg
-      })
+        type: "FAILURE",
+        payload: error.response.data.msg,
+      });
     }
-  }
+  };
 }
 
-export function getNameCountry (name) {
+export function getNameCountry(name) {
   return async function (dispatch) {
     try {
-      var json = await axios.get('http://localhost:3001/countries?name=' + name)
-      var resp = json.data
-      return dispatch({ type: 'GET_NAME_COUNTRY', payload: resp })
+      var json = await axios.get("/countries?name=" + name);
+      var resp = json.data;
+      return dispatch({ type: "GET_NAME_COUNTRY", payload: resp });
     } catch (error) {
       return dispatch({
-        type: 'FAILURE',
-        payload: error.response.data.msg
-      })
+        type: "FAILURE",
+        payload: error.response.data.msg,
+      });
     }
-  }
+  };
 }
 
-export function changePage (page) {
+export function changePage(page) {
   return {
-    type: 'CHANGE_PAGE',
-    payload: page
-  }
+    type: "CHANGE_PAGE",
+    payload: page,
+  };
 }
 
-export const getCountryDetail = id => {
-  return dispatch => {
+export const getCountryDetail = (id) => {
+  return (dispatch) => {
     return axios
-      .get(`http://localhost:3001/countries/${id}`)
-      .then(r => {
+      .get(`/countries/${id}`)
+      .then((r) => {
         return dispatch({
-          type: 'GET_COUNTRY_DETAIL',
-          payload: r.data
-        })
+          type: "GET_COUNTRY_DETAIL",
+          payload: r.data,
+        });
       })
 
-      .catch(err => {
-        alert(err)
-      })
-  }
-}
+      .catch((err) => {
+        alert(err);
+      });
+  };
+};
 
-export function postActivity (payload) {
-  console.log(payload)
+export function postActivity(payload) {
+  console.log(payload);
   return async function (dispatch) {
     try {
-      const res = await axios.post('http://localhost:3001/activity', payload)
+      const res = await axios.post("/activity", payload);
       return dispatch({
-        type: 'POST',
-        payload: res.data
-      })
+        type: "POST",
+        payload: res.data,
+      });
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 }
 
-export function getCountryId (id) {
+export function getCountryId(id) {
   return async function (dispatch) {
     try {
-      var json = await axios.get(`http://localhost:3001/countries/${id}`)
-      return dispatch({ type: 'GET_COUNTRY_ID', payload: json.data })
+      var json = await axios.get(`/countries/${id}`);
+      return dispatch({ type: "GET_COUNTRY_ID", payload: json.data });
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 }
 
-export function clearNameCountryForm () {
+export function clearNameCountryForm() {
   return function (dispatch) {
-    return dispatch({ type: 'CLEAR_NAME_COUNTRY_FORM' })
-  }
+    return dispatch({ type: "CLEAR_NAME_COUNTRY_FORM" });
+  };
 }
 
-export function byActivity (payload) {
+export function byActivity(payload) {
   return {
-    type: 'BY_ACTIVITY',
-    payload
-  }
+    type: "BY_ACTIVITY",
+    payload,
+  };
 }
 
-export function byContinent (payload) {
+export function byContinent(payload) {
   //funcion a la cual le voy a pasar el payload
   // console.log(payload)
   return {
-    type: 'BY_CONTINENT',
-    payload
-  }
+    type: "BY_CONTINENT",
+    payload,
+  };
 }
 
-export function byPoblation (payload) {
+export function byPoblation(payload) {
   return {
-    type: 'BY_POBLATION',
-    payload
-  }
+    type: "BY_POBLATION",
+    payload,
+  };
 }
 
-export function byOrder (payload) {
+export function byOrder(payload) {
   return {
-    type: 'BY_ORDER',
-    payload
-  }
+    type: "BY_ORDER",
+    payload,
+  };
 }
 
-export function setContinent (continent) {
+export function setContinent(continent) {
   return {
-    type: 'SET_CONTINENT',
-    payload: continent
-  }
+    type: "SET_CONTINENT",
+    payload: continent,
+  };
 }
 
 // export function byPopulation (payload) {
 //     return async function (dispatch) {
-//         var json = await axios.get(`http://localhost:3001/countries/`)
+//         var json = await axios.get(`/countries/`)
 //     }
 // }
